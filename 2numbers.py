0194a03e1869db953bfd5c26d2dbacff1d1ca3fe
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-05-30 13:07:13 +0400

    added coments

--- 2numbers.py
+++ 2numbers.py
@@ -1,10 +1,20 @@
+from xml.etree.ElementTree import Comment
+
+
 number_1 = 30
 number_2 = 8
 
+#
 print (f"{number_1} * {number_2} = {number_1 * number_2}")
+#
 print (f"{number_1} + {number_2} = {number_1 + number_2}")
+#
 print (f"{number_1} - {number_2} = {number_1 - number_2}")
+#
 print (f"{number_1} / {number_2} = {number_1 / number_2}")
+#
 print (f"{number_1} // {number_2} = {number_1 // number_2}")
+#
 print (f"{number_1} % {number_2} = {number_1 % number_2}")
+#
 print (f"{number_1} ** {number_2} = {number_1 ** number_2}")
