eedef631d3a8b3f4a8a59cf17822067e2970f637
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-05-30 13:09:25 +0400

    added comments

--- 2numbers.py
+++ 2numbers.py
@@ -4,17 +4,17 @@ from xml.etree.ElementTree import Comment
 number_1 = 30
 number_2 = 8
 
-#
+#product
 print (f"{number_1} * {number_2} = {number_1 * number_2}")
-#
+#sum
 print (f"{number_1} + {number_2} = {number_1 + number_2}")
-#
+#subtract
 print (f"{number_1} - {number_2} = {number_1 - number_2}")
-#
+#division
 print (f"{number_1} / {number_2} = {number_1 / number_2}")
-#
+#remainder
 print (f"{number_1} // {number_2} = {number_1 // number_2}")
-#
+#remainder
 print (f"{number_1} % {number_2} = {number_1 % number_2}")
-#
+#exponentiation
 print (f"{number_1} ** {number_2} = {number_1 ** number_2}")
