7a7af0f35946b6582fbcc59f70e1079aefac99cd
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-05-30 15:23:05 +0400

    input numbers

--- 2numbers.py
+++ 2numbers.py
@@ -1,20 +1,32 @@
 from xml.etree.ElementTree import Comment
 
 
-number_1 = 30
-number_2 = 8
+num1 = input("Please, input 1st number \n")
+number_1 = int(num1)
+#print(number_1)
+num2 = input("Please, input 1st number \n")
+number_2 = int(num2)
+#print(number_2)
+
+
 
 #product
 print (f"{number_1} * {number_2} = {number_1 * number_2}")
+
 #sum
 print (f"{number_1} + {number_2} = {number_1 + number_2}")
+
 #subtract
 print (f"{number_1} - {number_2} = {number_1 - number_2}")
+
 #division
 print (f"{number_1} / {number_2} = {number_1 / number_2}")
+
 #remainder
 print (f"{number_1} // {number_2} = {number_1 // number_2}")
+
 #remainder
 print (f"{number_1} % {number_2} = {number_1 % number_2}")
+
 #exponentiation
 print (f"{number_1} ** {number_2} = {number_1 ** number_2}")
