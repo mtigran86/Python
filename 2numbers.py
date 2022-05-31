1541df35bfcf517b61898c8b82c2f203ba2cba73
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-05-31 16:17:07 +0400

    if case added

--- 2numbers.py
+++ 2numbers.py
@@ -1,27 +1,26 @@
 num1 = input("Please, input 1st number \n")
-number_1 = int(num1)
-#print(number_1)
-num2 = input("Please, input 1st number \n")
-number_2 = int(num2)
-#print(number_2)
+num2 = input("Please, input 2nd number \n")
 
-#product
-print (f"{number_1} * {number_2} = {number_1 * number_2}")
+if num1.isdigit and num2.isdigit:
+    number_1 = int(num1)
+    number_2 = int(num2)
+    #product
+    print(f"{number_1} * {number_2} = {number_1 * number_2}")
+    #sum
+    print(f"{number_1} + {number_2} = {number_1 + number_2}")
+    #subtract
+    print(f"{number_1} - {number_2} = {number_1 - number_2}")
+    #exponentiation
+    print(f"{number_1} ** {number_2} = {number_1 ** number_2}")
+else:
+    print("Please, input only numbers")
 
-#sum
-print (f"{number_1} + {number_2} = {number_1 + number_2}")
-
-#subtract
-print (f"{number_1} - {number_2} = {number_1 - number_2}")
-
-#division
-print (f"{number_1} / {number_2} = {number_1 / number_2}")
-
-#remainder
-print (f"{number_1} // {number_2} = {number_1 // number_2}")
-
-#remainder
-print (f"{number_1} % {number_2} = {number_1 % number_2}")
-
-#exponentiation
-print (f"{number_1} ** {number_2} = {number_1 ** number_2}")
+if num2 ==0:
+    print("Devision by 0 !")
+else:
+    #division
+    print(f"{number_1} / {number_2} = {number_1 / number_2}")
+    #remainder
+    print(f"{number_1} // {number_2} = {number_1 // number_2}")    
+    #remainder
+    print(f"{number_1} % {number_2} = {number_1 % number_2}")
