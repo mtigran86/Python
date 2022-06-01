95a77205ec2ef0eae898b9d7661b99b8912c4352
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-06-01 19:41:35 +0400

    all working ;-)

--- 2numbers.py
+++ 2numbers.py
@@ -1,9 +1,11 @@
 num1 = input("Please, input 1st number \n")
 num2 = input("Please, input 2nd number \n")
 
-if num1.isdigit and num2.isdigit:
-    number_1 = int(num1)
-    number_2 = int(num2)
+
+
+if num1.isdigit() and num2.isdigit():
+    number_1=int(num1)
+    number_2=int(num2)
     #product
     print(f"{number_1} * {number_2} = {number_1 * number_2}")
     #sum
@@ -12,15 +14,14 @@ if num1.isdigit and num2.isdigit:
     print(f"{number_1} - {number_2} = {number_1 - number_2}")
     #exponentiation
     print(f"{number_1} ** {number_2} = {number_1 ** number_2}")
+    if number_2 != 0:
+        #division
+        print(f"{number_1} / {number_2} = {number_1 / number_2}")
+        #remainder
+        print(f"{number_1} // {number_2} = {number_1 // number_2}")
+        #remainder
+        print(f"{number_1} % {number_2} = {number_1 % number_2}")
+    else:
+        print("\n---===   Devision by 0   ===---")
 else:
-    print("Please, input only numbers")
-
-if num2 ==0:
-    print("Devision by 0 !")
-else:
-    #division
-    print(f"{number_1} / {number_2} = {number_1 / number_2}")
-    #remainder
-    print(f"{number_1} // {number_2} = {number_1 // number_2}")    
-    #remainder
-    print(f"{number_1} % {number_2} = {number_1 % number_2}")
+    print("\n ---===   Please, input only integers   ===---")
