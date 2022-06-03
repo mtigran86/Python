2fbeab4a3cebb53a4b81da189a0f24c23922ac77
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-06-03 13:56:39 +0400

    task 3 completed

--- task_3.py
+++ task_3.py
@@ -1,5 +1,15 @@
 string_to_cut = input("Please, input some text here\n")
-num_of_sym_to_cut = input("Please, input number\n")
+num = input("Please, input number\n")
 
-if num_of_sym_to_cut.isdigit():
-    
+try:
+    num_of_sym_to_cut = int(num)
+    new_text = string_to_cut[num_of_sym_to_cut:]
+    if num_of_sym_to_cut > len(string_to_cut):
+        print("\n---===   Тhe entered number is greater than the number of characters in the string   ===---")
+    elif num_of_sym_to_cut < 0:
+        print("\n---===   Тhe entered number is negative.   ===---")
+        print(f"Result: {new_text}")
+    else:
+        print(f"\nResult: {new_text}")
+except ValueError:
+    print("\n---===   Please, input only integers   ===---")
