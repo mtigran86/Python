ae3d630d6d41b56ff6b2c77ab8dea4e432854223
Tigran Musayelyan <tigran.musayelyan@outlook.com>
2022-06-03 13:32:29 +0400

    some corrections in 2nd task

--- task_2.py
+++ task_2.py
@@ -1,13 +1,12 @@
 user_input = input("Please input some text and press <ENTER>\n")
 
 output = ""
-my_list = list(user_input)
 string_len = len(user_input)
 x = 0
 
 while x < string_len:
     if x % 2:
-        output = output + my_list[x]
+        output = output + user_input[x]
     x+=1
 
 print(output)
---
+++ task_3.py
@@ -0,0 +1,5 @@
+string_to_cut = input("Please, input some text here\n")
+num_of_sym_to_cut = input("Please, input number\n")
+
+if num_of_sym_to_cut.isdigit():
+    
